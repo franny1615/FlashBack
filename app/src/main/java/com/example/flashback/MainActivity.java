6f76d2e414e24099198f56cfd0660771b49df53f
franke <franciscofunes5@gmail.com>
2021-04-03 11:05:39 -0700

    minor updates to mainactivity, strings, editflashcard

--- app/src/main/java/com/example/flashback/MainActivity.java
+++ app/src/main/java/com/example/flashback/MainActivity.java
@@ -2,30 +2,14 @@ package com.example.flashback;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
-
-import com.example.flashback.EditCard.EditFlashCardActivity;
 
 public class MainActivity extends AppCompatActivity {
+    public static String EXTRA_FLASHCARD_ID = "com.example.flashback.MainActivity.FlashcardID";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
-    public void openBoilerActivity(View view){
-        Intent intent = new Intent(this, BoilerPlateActivity.class);
-        startActivity(intent);
-    }
-
-    public void editFlashCard(View view)
-    {
-        Intent intent = new Intent(this, EditFlashCardActivity.class);
-        startActivity(intent);
-    }
-
-
 }
